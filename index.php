f0945f20473d0611988eb855fecf1ff703f170b7
Lorenzo <baccini.lorenzo@gmail.com>
2021-10-07 15:23:21 +0200

    Init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident beatae deserunt commodi optio quisquam consectetur officia? Repudiandae dolorem, neque sit natus nemo alias fuga laudantium veritatis cum tempora animi amet.";
+    $_GET["badword"];
+    
+
+?>
+
+<h2>Testo</h2>
+<p><?php echo $paragraph ?></p>
