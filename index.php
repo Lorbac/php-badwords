7fff7feae0a35ceaa3f393cf61637c06d6c98faf
Lorenzo <baccini.lorenzo@gmail.com>
2021-10-07 16:34:52 +0200

    Fine esercizio

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php 
 
     $paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident beatae deserunt commodi optio quisquam consectetur officia? Repudiandae dolorem, neque sit natus nemo alias fuga laudantium veritatis cum tempora animi amet.";
-    $_GET["badword"];
-    
+    $badword = $_GET["badword"];
+    $censured = "***";
+    $newParagraph = str_ireplace($badword, $censured, $paragraph);
 
 ?>
 
-<h2>Testo</h2>
+<h2>Testo non censurato</h2>
+<h3>Parola da censurare: <?php echo $badword ?></h3>
 <p><?php echo $paragraph ?></p>
+<hr>
+
+<h2>Testo censurato</h2>
+<p><?php echo $newParagraph ?></p>
